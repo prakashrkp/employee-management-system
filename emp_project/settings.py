2944e36758828dce12fb9d351d55024f161fa2f0
Prakash <kadambakudiprakash@gmail.com>
2026-07-15 09:33:06 +0530

    Switch to PostgreSQL for production

--- emp_project/settings.py
+++ emp_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x!%39965r$1e)9-hsn13#@7cg7i@(pu=c(g42g633v8heyyh2#'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-x!%39965r$1e)9-hsn13#@7cg7i@(pu=c(g42g633v8heyyh2#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = [
     'employee-management-system-1-jnh9.onrender.com',
@@ -75,16 +77,11 @@ WSGI_APPLICATION = 'emp_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'emp_db',
-        'USER': 'root',
-        'PASSWORD': 'Apex@3244',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+    )
 }
 
 
